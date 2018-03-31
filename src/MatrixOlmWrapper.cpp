bd49f0cf15a37cf9dc94fb2d14af6c40337a38b4
ajberchek <ajberchek@gmail.com>
2018-03-31 00:24:19 -0700

    Changed mallocs to calls to new and byte buffers are now wrapped by unique_ptr

--- src/MatrixOlmWrapper.cpp
+++ src/MatrixOlmWrapper.cpp
@@ -1,6 +1,7 @@
 #include "MatrixOlmWrapper.hpp"
 
 #include <chrono>
+#include <memory>
 #include <thread>
 
 #include <stdlib.h>
@@ -10,30 +11,22 @@
 #endif
 
 // buffer_size is the size of the buffer in number of bytes
-void *getRandData(unsigned int buffer_size) {
-  unsigned char *buffer = (unsigned char *)malloc(buffer_size);
-  if (!buffer) {
-    return nullptr;
-  }
+std::unique_ptr<uint8_t[]> getRandData(unsigned int buffer_size) {
+  std::unique_ptr<uint8_t[]> buffer(new uint8_t[buffer_size]);
 
-    // TODO: add other options to generate random data for different OSs
+  // TODO: add other options to generate random data for different OSs
 #ifdef __linux__
   int fd;
   if ((fd = open("/dev/random", O_RDONLY)) != -1) {
     unsigned int bytes_read = 0;
     while (bytes_read < buffer_size) {
-      unsigned int numRead =
-          read(fd, &buffer[bytes_read], buffer_size - bytes_read);
-      bytes_read += numRead;
-      if (numRead != 0) {
-        std::cout << "read a total of " << bytes_read << " bytes" << std::endl;
-      }
+      bytes_read +=
+          read(fd, &(buffer.get()[bytes_read]), buffer_size - bytes_read);
     }
     close(fd);
     return buffer;
   } else {
     std::cout << "Failed to open /dev/random" << std::endl;
-    free(buffer);
     return nullptr;
   }
 #endif
@@ -43,12 +36,12 @@ void MatrixOlmWrapper::setupIdentityKeys() {
   if (!id_published) {
     if (identity_keys_.empty()) {
       int id_buff_size = olm_account_identity_keys_length(acct);
-      void *id_buff = malloc(id_buff_size);
-      if (id_buff && olm_error() != olm_account_identity_keys(acct, id_buff,
-                                                              id_buff_size)) {
-        identity_keys_ = std::string(static_cast<char *>(id_buff));
+      std::unique_ptr<uint8_t[]> id_buff(new uint8_t[id_buff_size]);
+      if (olm_error() !=
+          olm_account_identity_keys(acct, id_buff.get(), id_buff_size)) {
+        identity_keys_ =
+            std::string(reinterpret_cast<const char *>(id_buff.get()));
       }
-      free(id_buff);
     }
 
     // Form json and publish keys
@@ -79,7 +72,6 @@ void MatrixOlmWrapper::setupIdentityKeys() {
 // "<curve25519_key>"}
 json MatrixOlmWrapper::signKey(json &key) {
   int sig_length, message_length;
-  void *sig;
   void const *message;
   json to_sign, signed_key;
 
@@ -90,13 +82,17 @@ json MatrixOlmWrapper::signKey(json &key) {
   message_length = m.size();
   message = static_cast<void const *>(m.c_str());
   sig_length = olm_account_signature_length(acct);
+  std::unique_ptr<uint8_t[]> sig(new uint8_t[sig_length]);
 
-  if ((sig = malloc(sig_length)) != 0 && olm_error() != olm_account_sign(acct, message, message_length, sig, sig_length)) {
-      std::string signature = std::string(static_cast<char *>(sig));
-      signed_key = {{"signed_curve25519:" + key.begin().key(), {{to_sign.begin().key(), to_sign.begin().value()},{"signatures",{{user_id_, {{"ed25519:" + device_id_, signature}}}}}}}};
+  if (olm_error() !=
+      olm_account_sign(acct, message, message_length, sig.get(), sig_length)) {
+    std::string signature =
+        std::string(reinterpret_cast<const char *>(sig.get()));
+    signed_key = {{"signed_curve25519:" + key.begin().key(),
+                   {{to_sign.begin().key(), to_sign.begin().value()},
+                    {"signatures",
+                     {{user_id_, {{"ed25519:" + device_id_, signature}}}}}}}};
   }
-
-  free(sig);
   return signed_key;
 }
 
@@ -104,31 +100,30 @@ json MatrixOlmWrapper::signKey(json &key) {
 // Upon successful return, data will contain the signed keys
 int MatrixOlmWrapper::genSignedKeys(json &data, int num_keys) {
   int generated_keys = 0;
-  void *rand_data;
-  int rand_length = olm_account_generate_one_time_keys_random_length(acct, num_keys);
-    data = json::parse("{}");
+  int rand_length =
+      olm_account_generate_one_time_keys_random_length(acct, num_keys);
+  std::unique_ptr<uint8_t[]> rand_data = getRandData(rand_length);
+  data = json::parse("{}");
 
-  if ((rand_data = getRandData(rand_length)) != nullptr && olm_error() != olm_account_generate_one_time_keys(acct, num_keys, rand_data, rand_length)) {
-      int keys_size = olm_account_one_time_keys_length(acct);
-      void *keys = malloc(keys_size);
+  if (olm_error() != olm_account_generate_one_time_keys(
+                         acct, num_keys, rand_data.get(), rand_length)) {
+    int keys_size = olm_account_one_time_keys_length(acct);
+    std::unique_ptr<uint8_t[]> keys(new uint8_t[keys_size]);
 
-      if (keys && olm_error() != olm_account_one_time_keys(acct, keys, keys_size)) {
-        json one_time_keys = json::parse(std::string(static_cast<char *>(keys)));
-        for (auto it = one_time_keys["curve25519"].begin();
-             it != one_time_keys["curve25519"].end(); ++it) {
-          json key;
-          key[it.key()] = it.value();
-          json signed_key = signKey(key);
-          data["one_time_keys"][signed_key.begin().key()] =
-              signed_key.begin().value();
-        }
-        generated_keys = num_keys;
+    if (olm_error() != olm_account_one_time_keys(acct, keys.get(), keys_size)) {
+      json one_time_keys =
+          json::parse(std::string(reinterpret_cast<const char *>(keys.get())));
+      for (auto it = one_time_keys["curve25519"].begin();
+           it != one_time_keys["curve25519"].end(); ++it) {
+        json key;
+        key[it.key()] = it.value();
+        json signed_key = signKey(key);
+        data["one_time_keys"][signed_key.begin().key()] =
+            signed_key.begin().value();
       }
-
-      free(keys);
+      generated_keys = num_keys;
+    }
   }
-
-  free(rand_data);
   return generated_keys;
 }
 
@@ -136,7 +131,8 @@ int MatrixOlmWrapper::genSignedKeys(json &data, int num_keys) {
 void MatrixOlmWrapper::replenishKeyJob() {
   // Call upload keys to figure out how many keys are present
   std::string empty = "{}";
-  uploadKeys(empty, [this](const std::string &key_counts, std::experimental::optional<std::string>) {
+  uploadKeys(empty, [this](const std::string &key_counts,
+                           std::experimental::optional<std::string>) {
     int current_key_count = 0;
     if (!key_counts.empty() &&
         json::parse(key_counts).count("one_time_key_counts") == 1) {
@@ -145,17 +141,25 @@ void MatrixOlmWrapper::replenishKeyJob() {
               .get<int>();
     }
 
-    int keys_needed = static_cast<int>(olm_account_max_number_of_one_time_keys(acct)) - current_key_count;
-    // Used until new random number generation is in place since /dev/random takes a long time to generate random data
+    int keys_needed =
+        static_cast<int>(olm_account_max_number_of_one_time_keys(acct)) -
+        current_key_count;
+    // Used until new random number generation is in place since /dev/random
+    // takes a long time to generate random data
     keys_needed = 2;
 
     json data;
     if (genSignedKeys(data, keys_needed) > 0 && uploadKeys != nullptr) {
       std::string data_string = data.dump(2);
-      uploadKeys(data_string, [this, current_key_count](const std::string &resp, std::experimental::optional<std::string> err) {
+      uploadKeys(
+          data_string, [this, current_key_count](
+                           const std::string &resp,
+                           std::experimental::optional<std::string> err) {
             if (!err) {
-              std::cout << "publishing one time keys returned " << resp << std::endl;
-              if (json::parse(resp)["one_time_key_counts"]["signed_curve25519"].get<int>() > current_key_count) {
+              std::cout << "publishing one time keys returned " << resp
+                        << std::endl;
+              if (json::parse(resp)["one_time_key_counts"]["signed_curve25519"]
+                      .get<int>() > current_key_count) {
                 olm_account_mark_keys_as_published(acct);
               }
             }
@@ -168,36 +172,30 @@ OlmAccount *MatrixOlmWrapper::loadAccount(std::string keyfile_path,
                                           std::string keyfile_pass) {
   OlmAccount *acct;
   if (keyfile_path == "" && keyfile_pass == "") {
-    void *memory = malloc(olm_account_size());
-    if (memory) {
-      void *random;
-      int random_size;
+    std::unique_ptr<uint8_t[]> memory(new uint8_t[olm_account_size()]);
+    std::unique_ptr<uint8_t[]> random;
+    int random_size;
 
-      acct = olm_account(memory);
-      random_size = olm_create_account_random_length(acct);
-      if ((random = getRandData(random_size)) != nullptr &&
-          olm_error() != olm_create_account(acct, random, random_size)) {
+    acct = olm_account(memory.get());
+    random_size = olm_create_account_random_length(acct);
+    random = getRandData(random_size);
+    if (olm_error() != olm_create_account(acct, random.get(), random_size)) {
 
-        std::thread([this]() {
-          while (true) {
-            setupIdentityKeys();
-            if (id_published) {
-              replenishKeyJob();
-            }
-            std::this_thread::sleep_for(std::chrono::minutes(1));
+      std::thread([this]() {
+        while (true) {
+          setupIdentityKeys();
+          if (id_published) {
+            replenishKeyJob();
           }
-        }).detach();
+          std::this_thread::sleep_for(std::chrono::minutes(1));
+        }
+      })
+          .detach();
 
-        free(random);
-        return acct;
-      }
-
-      free(memory);
-      free(random);
-      return nullptr;
-    } else {
-      return nullptr;
+      return acct;
     }
+
+    return nullptr;
   } else {
     // Stubbed functionality
     return nullptr;
